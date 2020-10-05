4c880db828242064a347413953ffe310357726e6
Arnaud Castellanos Galea <poderosoarnaud@gmail.com>
2020-10-05 10:23:43 +0800

    Update Examples

--- examples/plugin.nim
+++ examples/plugin.nim
@@ -1,7 +1,10 @@
 # Compile with: nim c --noMain --app:lib plugin.nim
-proc hello(x: int) {.exportc.} =
+proc hello(x: int32) {.cdecl, exportc, dynlib.} =
   echo "Hello world, ", x
 
-proc highFive(): int {.exportc.} =
+proc highFive(): int32 {.cdecl, exportc, dynlib.} =
   result = 5
   echo "Here you go!"
+
+proc helloFancy(f: proc (n: int32): int32 {.cdecl.}) {.exportc.} =
+  echo "Such fancyness, ", f(42)
